31564746396f527eb6fb14ac89b77eec0b01033b
jadecodespy <jadekanimodo@gmail.com>
2020-07-20 16:10:01 +0100

    addedurl

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,8 +6,8 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    getnumbers = request.get('jadeazfunction.azurewebsites.net/api/HttpTrigger2?code=rFIbZDWG04Um4U1tQ1LtZkVuOrUda13UrMc7buN3nHCBu/BEGoniug==').text
-    getletters = request.get('jadeazfunction.azurewebsites.net/api/HttpTrigger3?code=3vYSLRruH2cdxNqFDmH1OW56C0BNmJpnENKhpuk08uwWR4bGbhU2Ig==').text
+    getnumbers = request.get('https://jadeazfunction.azurewebsites.net/api/HttpTrigger2?code=rFIbZDWG04Um4U1tQ1LtZkVuOrUda13UrMc7buN3nHCBu/BEGoniug==')
+    getletters = request.get('https://jadeazfunction.azurewebsites.net/api/HttpTrigger3?code=3vYSLRruH2cdxNqFDmH1OW56C0BNmJpnENKhpuk08uwWR4bGbhU2Ig==')
     
     numbers = getnumbers.text 
     letters = getletter.text
